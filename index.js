12d8ecc3c9be2f069ef3a23e693193d68cf5e7a2
RAO YANG <lightrao@hotmail.com>
2023-04-13 07:47:42 +0800

    Constants

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
-let myName;
-console.log(myName);
-myName = "ZhangSan";
-console.log(myName);
-
-let yourName = "LiSi";
-console.log(yourName);
-
-// Can not be a reserved keyword
-// let if="Hi";
-
-// Should be meaningful
 let myAge = 33;
-// let a = 33;
+myAge = 44;
+console.log(myAge);
 
-// Can not start with number
-// let 2name="WangWu";
-
-// Can not contain a space or hyphen (-)
-// let my name="ZhangSan";
-// let my-name="ZhangSan"
-
-// Use camel notation
-let firstName = "San";
-let lastName = "Zhang";
-
-// case-sensitive
-// let firstName="Si";
-let FirstName = "Si";
-
-// Declare multiple variables,
-// let width = 22,
-//   height = 33;
-let width = 22;
-let height = 33;
+const pi = 3.14;
+// pi = 4;
+console.log(pi);
