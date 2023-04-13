81df771621ba2740fc7ad3a83c1318f01a591c60
RAO YANG <lightrao@hotmail.com>
2023-04-13 22:30:35 +0800

    done

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function greet(name) {
 }
 greet("ZhangSan");
 greet("LiSi");
+
+// calculating a value
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+let result = addTwoNumbers(3, 5);
+console.log(result);
+
+// function in math
+function square(number) {
+  return number * number;
+}
+console.log(square(9));
