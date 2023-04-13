2bd381960237f3fa6ac375ff2645c5528b4b998f
RAO YANG <lightrao@hotmail.com>
2023-04-13 22:21:11 +0800

    Types of Functions

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+// performing a task
 function greet(name) {
   console.log("Hello " + name);
 }
 greet("ZhangSan");
 greet("LiSi");
 
+// calculating a value
 function addTwoNumbers(a, b) {
   return a + b;
 }
 let result = addTwoNumbers(3, 5);
 console.log(result);
+
+// function in math
+function square(number) {
+  return number * number;
+}
+console.log(square(9));
