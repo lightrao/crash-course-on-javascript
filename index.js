a2dc33ca934fb6f4dc59e56219620d2307a2a764
RAO YANG <lightrao@hotmail.com>
2023-04-13 08:38:41 +0800

    Dynamic Typing

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ let age = 33; // Number Literal
 let isApproved = true; // Boolean Literal, can be true or false
 let yourName = undefined;
 let selectedColor = null;
+
+console.log(typeof myName);
+myName = 1;
+console.log(typeof myName);
+
+// In Javascript just have number
+console.log(typeof age);
+age = 30.2;
+console.log(typeof age);
+
+console.log(typeof isApproved);
+console.log(typeof yourName); // type: undefined, value: undefined
+console.log(typeof selectedColor); // type: object, value: null
