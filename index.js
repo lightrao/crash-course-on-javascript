fa0b9d728d1a0acb985d5a29d41352347374f85c
RAO YANG <lightrao@hotmail.com>
2023-04-14 20:39:43 +0800

    Arithmetic Operators

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-// This is a comment
-console.log("Hello world!");
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+// Increment
+console.log(++x);
+
+console.log(x++);
+console.log(x);
+
+// Decrement
+console.log(--y);
+
+console.log(y--);
+console.log(y);
