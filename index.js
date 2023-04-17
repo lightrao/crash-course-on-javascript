975a3bd20665ba16e7fe0bc07575d6f178d82b10
RAO YANG <lightrao@hotmail.com>
2023-04-17 22:59:45 +0800

    Logical Operators with booleans

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const age = 26;
-const beverage = age >= 21 ? "Beer" : "Juice";
-console.log(beverage); 
+let bringWallet = true;
+let takeShower = true;
+let goDate = bringWallet && takeShower;
+console.log(goDate);
+
+let highIncome = false;
+let goodCredit = false;
+let eligibleForLoan = highIncome || goodCredit;
+console.log(eligibleForLoan);
+
+let lucky = true;
+let unlucky = !lucky;
+console.log(unlucky);
