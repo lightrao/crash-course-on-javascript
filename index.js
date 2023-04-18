193e21deb0bbc747fa93fa76bd1b781cb0ffae5e
RAO YANG <lightrao@hotmail.com>
2023-04-18 20:36:06 +0800

    Operator Precedence

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-// 1 = 00000001
-// 2 = 00000010
-// | = 00000011
-console.log(1 | 2);
+let x = 3 + 4 * 7;
+console.log(x);
 
-// 1 = 00000001
-// 2 = 00000010
-// & = 00000000
-console.log(1 & 2);
+let y = (3 + 4) * 7;
+console.log(y);
 
-// 1 = 00000001
-// 2 = 00000010
-// ^ = 00000011
-console.log(1 ^ 2);
+function sum(a, b) {
+    return a + b;
+}
 
-// read, write, execute
-// 00000100
-// 00000010
-// 00000001
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
-
-let myPermission = 0;
-
-// add permission
-myPermission = myPermission | readPermission | writePermission;
-console.log(myPermission);
-
-// check permission
-let message = (myPermission & executePermission) ? "yes" : "no";
-console.log(message);
+let z = 3 * sum(2, 4);
+console.log(z);
