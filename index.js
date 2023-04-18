59f7402b712a1966d8f26d6f321f778ceeb74b1c
RAO YANG <lightrao@hotmail.com>
2023-04-18 20:51:30 +0800

    Exercise-Swapping Variables

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-let x = 3 + 4 * 7;
-console.log(x);
+let x = "green";
+let y = "blue";
+console.log(x, y);
 
-let y = (3 + 4) * 7;
-console.log(y);
-
-function sum(a, b) {
-    return a + b;
-}
-
-let z = 3 * sum(2, 4);
-console.log(z);
+let z = x;
+x = y;
+y = z;
+console.log(x, y);
