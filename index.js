fe79e72bdcd9ae9743a6104951cc8a4e1232f5bf
RAO YANG <lightrao@hotmail.com>
2023-04-23 07:46:35 +0800

    while

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-for (let i = 1; i <= 5; i++) {
-    // console.log("Hello world!", i);
+let i = 0;
+while (i <= 5) {
     if (i % 2 !== 0) {
         console.log(i);
     }
-}
-
-for (let i = 5; i >= 1; i--) {
-    if (i % 2 !== 0) {
-        console.log(i);
-    }
+    i++;
 }
