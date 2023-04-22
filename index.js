696304caef6de57c4d20a290540228ed12584f47
RAO YANG <lightrao@hotmail.com>
2023-04-23 07:15:04 +0800

    If...else

---
+++ index.js
@@ -0,0 +1,9 @@
+let time = 19;
+
+if (time >= 6 && time < 12) {
+    console.log("Good morning");
+} else if (time >= 12 && time < 18) {
+    console.log("Good afternoon");
+} else {
+    console.log("Good evening");
+}
