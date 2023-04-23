02d979b4893d90eea1875dff1466ecd81f8f1ac7
RAO YANG <lightrao@hotmail.com>
2023-04-24 07:46:38 +0800

    for...of

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const person = {
-    name: "ZhangSan",
-    age: 33,
-    weight: 75,
-    height: 180
-};
-
-for (key in person) {
-    console.log(key, person[key]);
-}
-
 const colors = ['red', 'green', 'blue'];
-for (let index in colors)
-    console.log(index, colors[index]);
+
+for (const color of colors) {
+    console.log(color);
+}
