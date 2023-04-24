800954760587059461244bdafd900e2f7dc3ca49
RAO YANG <lightrao@hotmail.com>
2023-04-24 20:37:16 +0800

    Exercise- FizzBuzz

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function isLandscape(width, height) {
-    return (width > height);
+// Divisible by 3 => Fizz
+// Divisible by 5 => Buzz
+// Divisible by both 5 and 3 => FizzBuzz
+// Not divisible by 3 or 5 => same as input
+// Not a number => "Not a number"
+
+function fizzBuzz(input) {
+    if (typeof input !== "number") return "Not a number";
+    if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
+    if (input % 3 === 0) return "Fizz";
+    if (input % 5 === 0) return "Buzz";
+    return input;
 }
 
-console.log(isLandscape(300, 600));
+for (let i = 1; i <= 15; i++) {
+    console.log(i, fizzBuzz(i));
+}
+
+console.log(fizzBuzz("hi"));
