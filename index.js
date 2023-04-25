3a05d4ff780c0338d6893bbb426210a0eb5e3c88
RAO YANG <lightrao@hotmail.com>
2023-04-25 08:56:54 +0800

    Exercise- Even and Odd Numbers

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-// Speed Limit = 70km/h
-// every 5km/h above deduct 1 point
-// deduct 12 points lead to license suspended 
-
-function checkSpeed(speed) {
-    const speedLimit = 70;
-    const kmPerPoint = 5;
-
-    if (speed < speedLimit + kmPerPoint * 1) {
-        console.log("Ok.");
-    } else {
-        const points = Math.floor((speed - speedLimit) / 5);
-        if (points > 12) {
-            console.log("License suspended!");
-        } else {
-            console.log("Points deducted:", points);
-        }
+function showNumbers(input) {
+    for (let i = 1; i <= 10; ++i) {
+        const message = (i % 2 === 0) ? "EVEN" : "ODD";
+        console.log(i, message);
     }
 }
 
-for (let speed = 65; speed <= 140; speed += 5) {
-    checkSpeed(speed);
-}
+showNumbers(10);
