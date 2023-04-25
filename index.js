ab0f1f4183e86fb425b5a83ef838d3768b8537ed
RAO YANG <lightrao@hotmail.com>
2023-04-25 21:29:40 +0800

    Exercise - Stars

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-// Average score:
-// 1-59: F
-// 60-69: D
-// 70-79: C
-// 80-89: B
-// 90-100: A
-
-function calculateAverage(array) {
-    let sum = 0;
-    for (let value of array) {
-        sum += value;
+function showStars(rows) {
+    for (let row = 1; row <= rows; row++) {
+        let pattern = '';
+        for (let j = 1; j <= row; j++)
+            pattern += "*";
+        console.log(pattern);
     }
-    return sum / array.length;
 }
 
-function calculateGrade(marks) {
-    const average = calculateAverage(marks);
-    if (average < 60) return "F";
-    if (average < 70) return "D";
-    if (average < 80) return "C";
-    if (average < 90) return "B";
-    return "A";
-}
-
-const marks = [60, 75, 88];
-
-console.log(calculateGrade(marks));
+showStars(3);
