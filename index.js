7cb3944a6a46adc468f941ee7f09c94177b4adc6
RAO YANG <lightrao@hotmail.com>
2023-04-25 09:19:53 +0800

    Exercise- Count Truthy

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-function showNumbers(input) {
-    for (let i = 1; i <= 10; ++i) {
-        const message = (i % 2 === 0) ? "EVEN" : "ODD";
-        console.log(i, message);
+// Falsy: undefined, null, '', false, 0, NaN
+
+function countTruthy(array) {
+    let counter = 0;
+    for (element of array) {
+        if (element) counter++;
     }
+    return counter;
 }
 
-showNumbers(10);
+const myArray = [1, 2, 3, undefined, '', false, 8, true];
+console.log(countTruthy(myArray));
