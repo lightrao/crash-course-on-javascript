736ad6fab6285c50a9552f71b97f1d4aa783cd74
RAO YANG <lightrao@hotmail.com>
2023-04-27 07:43:46 +0800

    Exercise - Prime Numbers

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function showPrimes(limit) {
-
+function isPrime(number) {
+    for (let factor = 2; factor < number; factor++) {
+        if (number % factor === 0) return false;
+    }
+    return true;
 }
+
+function showPrimes(limit) {
+    for (let number = 2; number <= limit; number++) {
+        if (isPrime(number)) console.log(number);
+    }
+}
+
+showPrimes(20);
