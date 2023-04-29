9a3ac3f57fab2dc7e43295a8b004942555065760
RAO YANG <lightrao@hotmail.com>
2023-04-29 22:45:32 +0800

    Factory Functions

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-// OOP
-const circle = {
-    radius: 3,
-    location: {
-        x: 1,
-        y: 2
-    },
-    isVisible: true,
-    draw: function () {
-        console.log("draw circle");
-    },
-    move: function () {
-        console.log("move circle");
-    }
-};
+// const circle = {
+//     radius: 3,
+//     draw: function () {
+//         console.log("draw circle");
+//     },
+// };
 
-circle.draw();// method
+// const anotherCircle = {
+//     radius: 4,
+//     draw: function () {
+//         console.log("draw circle");
+//     },
+// };
+
+// console.log(circle);
+// console.log(anotherCircle);
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+            console.log("draw circle");
+        }
+    };
+}
+
+const circle = createCircle(3);
+console.log(circle);
+const anotherCircle = createCircle(4);
+console.log(circle);
