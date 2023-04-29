be91e31b59dd33902cc183e3e42dc49676944d46
RAO YANG <lightrao@hotmail.com>
2023-04-29 22:41:33 +0800

    simple circle

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 // OOP
 const circle = {
     radius: 3,
-    location: {
-        x: 1,
-        y: 2
-    },
-    isVisible: true,
     draw: function () {
         console.log("draw circle");
     },
-    move: function () {
-        console.log("move circle");
-    }
 };
 
 circle.draw();// method
