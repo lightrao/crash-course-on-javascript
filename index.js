f20299797c71903fce62ebfc03981decf933d603
RAO YANG <lightrao@hotmail.com>
2023-04-30 11:36:38 +0800

    Constructor Functions

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-// const circle = {
-//     radius: 3,
-//     draw: function () {
-//         console.log("draw circle");
-//     },
-// };
+// Factory Function
+// function createCircle(radius) {
+//     return {
+//         radius,
+//         draw() {
+//             console.log("draw circle");
+//         }
+//     };
+// }
 
-// const anotherCircle = {
-//     radius: 4,
-//     draw: function () {
-//         console.log("draw circle");
-//     },
-// };
-
+// const circle = createCircle(3);
 // console.log(circle);
-// console.log(anotherCircle);
+// const anotherCircle = createCircle(4);
+// console.log(circle);
 
-function createCircle(radius) {
-    return {
-        radius,
-        draw() {
-            console.log("draw circle");
-        }
+// Camel Notation: oneTwoThreeFour
+// Pascal Notation: OneTwoThreeFour
+
+// Constructor Functions
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log("draw circle");
     };
 }
 
-const circle = createCircle(3);
+// javascript new operator:
+// 1. new operator first creates an empty object
+// 2. new will set "this" to point to the object, and add properties with constructor
+// 3. new will return the object from consstructor function
+const circle = new Circle(3);
 console.log(circle);
-const anotherCircle = createCircle(4);
-console.log(circle);
+const anotherCircle = new Circle(4);
+console.log(anotherCircle);
