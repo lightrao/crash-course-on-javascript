e64f1fa14205bf239b5087924be7daf9b72e9451
RAO YANG <lightrao@hotmail.com>
2023-04-30 11:49:25 +0800

    Dynamic Nature of Objects

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
-// Factory Function
-// function createCircle(radius) {
-//     return {
-//         radius,
-//         draw() {
-//             console.log("draw circle");
-//         }
-//     };
-// }
+const circle = {
+    radius: 1
+};
 
-// const circle = createCircle(3);
-// console.log(circle);
-// const anotherCircle = createCircle(4);
-// console.log(circle);
+circle.color = "yellow";
+circle.draw = function () {
+    console.log("draw circle");
+};
 
-// Camel Notation: oneTwoThreeFour
-// Pascal Notation: OneTwoThreeFour
+delete circle.color;
+delete circle.draw;
 
-// Constructor Functions
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function () {
-        console.log("draw circle");
-    };
-}
-
-// javascript new operator:
-// 1. new operator first creates an empty object
-// 2. new will set "this" to point to the object, and add properties with constructor
-// 3. new will return the object from consstructor function
-const circle = new Circle(3);
 console.log(circle);
-const anotherCircle = new Circle(4);
-console.log(anotherCircle);
+
+// circle = { radius: 3 };
