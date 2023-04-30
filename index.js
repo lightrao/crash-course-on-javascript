08db8e8047281fe891c3744ee4c4643d31bf8604
RAO YANG <lightrao@hotmail.com>
2023-04-30 08:03:30 +0800

    adjust content

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let i = 0;
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+let i = 1;
 while (i <= 5) {
-    if (i % 2 !== 0) {
-        console.log(i);
-    }
+    // if (i % 2 !== 0) {
+    //     console.log(i);
+    // }
+    console.log(i);
     i++;
 }
