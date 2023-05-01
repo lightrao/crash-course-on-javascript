f710758715218ed667fea4b88f57b84929dd4638
RAO YANG <lightrao@hotmail.com>
2023-05-01 18:04:47 +0800

    Functions are Objects

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-// Factory Function
-function createCircle(radius) {
-    return {
-        radius,
-        draw: function () {
-            console.log("draw circle");
-        }
-    };
-}
-const circle = createCircle(2);
-console.log(circle.constructor);
-
-// Constructor Function
 function Circle(radius) {
     this.radius = radius;
-    this.draw = function () { console.log("draw circle"); };
+    this.draw = function () {
+        console.log("draw circle");
+    };
 }
-const anotherCircle = new Circle(2);
-console.log(anotherCircle.constructor);
 
-const person = {};
-// const person = new Object();
-const s = 'hello';
-// const s = new String('hello');
-const b = true;
-// const b = new Boolean(true);
-const n = 3
-// const n = new Number(3);
+const circle = new Circle(2);
+console.log(circle);
+
+console.log(Circle.name);
+console.log(Circle.length); // number of arguments
+console.log(Circle.constructor);
+
+// const Circle1 = new Function('radius', `
+// this.radius = radius;
+// this.draw = function () {
+//     console.log("draw circle");
+//  };
+// `);
+// const circle1 = new Circle1(2);
+// console.log(circle1);
+
+const anotherCircle = {};
+// Circle.call(anotherCircle, 2);
+Circle.apply(anotherCircle, [2]);
+console.log(anotherCircle);
+
+
+
