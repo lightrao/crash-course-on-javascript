41b7113c7fd1e927bae6a90feae596558e919b4f
RAO YANG <lightrao@hotmail.com>
2023-05-02 16:47:12 +0800

    String

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-console.log(Math.random());
-console.log(Math.round(1.5));
-console.log(Math.max(1, 2, 3, 4, 5));
-console.log(Math.min(-1, -2, -3, -4, -5));
+// String primitive
+const myString = 'hi';
+console.log(typeof myString);
+
+// String object
+const mySecondString = new String('hi');
+console.log(typeof mySecondString);
+
+
+const message = "This is my first message";
+console.log(message.length);
+console.log(message[0]);
+console.log(message[1]);
+console.log(message.includes('my'));
+console.log(message.includes('good'));
+console.log(message.startsWith('Thi'));
+console.log(message.endsWith('age'));
+console.log(message.indexOf('is'));
+console.log(message.replace('first', 'second'));
+console.log(message.toUpperCase());
+console.log(message.split(' '));
+
+const message2 = '   Hello world!   ';
+console.log(message2.trim());
+
+// escape notation
+const message3 = 'I\'m a \ngood man';
+console.log(message3);
+
