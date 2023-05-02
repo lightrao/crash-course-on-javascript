7b06ce71193e79051d607e53a433d49cc2483536
RAO YANG <lightrao@hotmail.com>
2023-05-02 12:00:57 +0800

    Math

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-let person = {
-    name: "Neo",
-    age: 23
-};
-console.log(person);
-
-
-
-
+console.log(Math.random());
+console.log(Math.round(1.5));
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(-1, -2, -3, -4, -5));
