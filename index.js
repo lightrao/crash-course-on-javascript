961a8dc80f84f116bc6e0de37eeb30cc5d003320
RAO YANG <lightrao@hotmail.com>
2023-05-03 00:27:34 +0800

    Date

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const message = "Stay \'hungry\'\nstay \'foolish\'";
-console.log(message);
+const now = new Date();
+// now.setFullYear(2024);
+// now.setDate(5);
+console.log(now);
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(now.toISOString());
 
-const templateMessage = `Stay 'hungry'
-Stay 'foolish'`;
-console.log(templateMessage);
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getDay());
 
-const myName = 'Neo';
-const placeholderMessage = `My name is ${myName}, my age is ${2023 - 2002}`;
-console.log(placeholderMessage);
+const date1 = new Date('May 11 2024 09:00');
+console.log(date1);
+const date2 = new Date(2024, 4, 11, 9);
+console.log(date2);
