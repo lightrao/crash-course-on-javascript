1abe85aa10458020e3265a0d96b5055faab10356
RAO YANG <lightrao@hotmail.com>
2023-05-02 11:20:02 +0800

    Garbage Collection

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-const circle = {
-    radius: 1,
-    draw() {
-        console.log("draw circle");
-    }
+let person = {
+    name: "Neo",
+    age: 23
 };
-console.log(circle);
-
-
-// const anotherCircle = {};
-// for (let key in circle) {
-//     anotherCircle[key] = circle[key];
-// }
-// console.log(anotherCircle)
-
-// anotherCircle['radius'] = circle['radius'];
-// anotherCircle['draw'] = circle['draw'];
-// console.log(anotherCircle)
-
-// const anotherCircle = Object.assign({}, circle);
-// console.log(anotherCircle)
-
-const anotherCircle = { ...circle };
-console.log(anotherCircle)
-
+console.log(person);
 
 
 
