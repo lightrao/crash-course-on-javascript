43656a3d0d19f5960c8ffa11daca8bfc443760ab
RAO YANG <lightrao@hotmail.com>
2023-05-04 07:04:06 +0800

    Address Object

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const now = new Date();
-// now.setFullYear(2024);
-// now.setDate(5);
-console.log(now);
-console.log(now.toDateString());
-console.log(now.toTimeString());
-console.log(now.toISOString());
+// street
+// city
+// zipCode
+// showAddress(address)
 
-console.log(now.getHours());
-console.log(now.getMinutes());
-console.log(now.getDay());
+const address = {
+    street: "BaiYun Road",
+    city: "KunMing",
+    zipCode: 650024,
+};
 
-const date1 = new Date('May 11 2024 09:00');
-console.log(date1);
-const date2 = new Date(2024, 4, 11, 9);
-console.log(date2);
+function showAddress(address) {
+    for (let key in address) {
+        console.log(`${key} is ${address[key]}.`);
+    }
+}
+
+showAddress(address);
