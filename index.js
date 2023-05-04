4b1f3eb5144dd5c030eb49527bf46c7accb463d8
RAO YANG <lightrao@hotmail.com>
2023-05-04 08:23:47 +0800

    Object Equality

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-// street
-// city
-// zipCode
-// showAddress(address)
-
-// Object literal
-// const address = {
-//     street: "BaiYun Road",
-//     city: "KunMing",
-//     zipCode: 650024,
-// };
-
-// Factory
-// function createAddress(street, city, zipCode) {
-//     return {
-//         street,
-//         city,
-//         zipCode,
-//     };
-// }
-
-// const address = createAddress("BaiYun Road", "KunMing", 650224);
-// console.log(address);
-
 // Consturctor
 function Address(street, city, zipCode) {
     this.street = street;
@@ -29,5 +5,37 @@ function Address(street, city, zipCode) {
     this.zipCode = zipCode;
 }
 
-const address = new Address("BaiYun Road", "KunMing", 650224);
-console.log(address);
+const address1 = new Address("BaiYun Road", "KunMing", 650224);
+const address2 = new Address("LongQuan Road", "KunMing", 650224);
+const address3 = address1;
+
+function areEqual(obj1, obj2) {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < keys1.length; i++) {
+        if (keys1[i] !== keys2[i]) {
+            return false;
+        }
+    }
+
+    for (let key of keys1) {
+        if (obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function areSame(obj1, obj2) {
+    return obj1 === obj2;
+}
+
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+console.log(areSame(address1, address3));
