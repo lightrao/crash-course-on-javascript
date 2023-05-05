a0715967c5fb4d86eb67e8febfb6db88b0ceb517
RAO YANG <lightrao@hotmail.com>
2023-05-06 07:13:42 +0800

    Adding Elements

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-// This is a comment
-console.log("Hello world!");
+const numbers = [3, 4];
+// numbers = [];
+
+// End
+numbers.push(5, 6);
+console.log(numbers);
+
+// Beginning
+numbers.unshift(1, 2);
+console.log(numbers);
+
+// Middle
+numbers.splice(3, 0, 3.3, 3.4);
+console.log(numbers);
