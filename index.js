9e03ddacc69129e2f9a072cce0d0e09275c27186
RAO YANG <lightrao@hotmail.com>
2023-05-05 08:22:49 +0800

    Price Range Objects

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-// let post = {
-//     title: "How to learn javascript",
-//     body: "1. Take a course. 2. learn from books. ...",
-//     author: "ZhangSan",
-//     views: 101,
-//     comments: [
-//         { author: "LiSi", body: "A good post." },
-//         { author: "WangWu", body: "Very useful." }
-//     ],
-//     isLive: true
-// };
+const ranges = [
+    { from: 0, to: 10, tip: "Inexpensive" },
+    { from: 11, to: 20, tip: "Moderate" },
+    { from: 21, to: 50, tip: "Expensive" }
+];
 
-function Post(title, body, author) {
-    this.title = title;
-    this.body = body;
-    this.author = author;
-    this.views = 0;
-    this.comments = [];
-    this.isLive = false;
+function checkPrice(price, ranges) {
+    let tip = 'No matching range found';
+    for (let i = 0; i < ranges.length; i++) {
+        const r = ranges[i];
+        if (price >= r.from && price <= r.to) {
+            tip = r.tip;
+            break;
+        }
+    }
+    console.log(`Price: ${tip}`);
 }
 
-const post = new Post("How to learn javascript", "This is post body...", "ZhangSan");
-console.log(post);
+checkPrice(11, ranges)
+
+
+
