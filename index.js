f489fb0b4397d1bea146d8aac2af6d0051fc5676
RAO YANG <lightrao@hotmail.com>
2023-05-06 07:45:57 +0800

    Finding Elements (Reference Types)

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const numbers = [1, 2, 3, 4];
-console.log(numbers.indexOf(2));
-console.log(numbers.indexOf(7));
-console.log(numbers.indexOf('2'));
+const languages = [
+    { id: 1, name: 'Javascript' },
+    { id: 2, name: 'Python' },
+    { id: 3, name: 'Swift' },
+    { id: 4, name: 'Solidity' },
+];
 
-const numbers2 = [1, 2, 3, 1, 1, 4];
-console.log(numbers2.indexOf(1));
-console.log(numbers2.lastIndexOf(1));
+// two different reference, two different object
+console.log(languages.includes({ id: 1, name: 'Javascript' }));
 
-// numbers2.indexOf(1) !== -1
-if (numbers2.includes(1)) {
-    console.log("numbers2 include 1.");
-}
-
-console.log(numbers2.indexOf(1, 1));
+// find, findIndex
+const languageBeFinded = languages.findIndex(function (language) {
+    return language.name === 'C#';
+});
+console.log(languageBeFinded);
