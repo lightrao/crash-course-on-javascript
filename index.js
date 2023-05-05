dff5163bdcb5a635989fa586ca7e93e5107cfd26
RAO YANG <lightrao@hotmail.com>
2023-05-06 07:53:15 +0800

    Arrow Functions

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const languages = [
-    { id: 1, name: 'Javascript' },
-    { id: 2, name: 'Python' },
-    { id: 3, name: 'Swift' },
-    { id: 4, name: 'Solidity' },
-];
+// const languages = [
+//     { id: 1, name: 'Javascript' },
+//     { id: 2, name: 'Python' },
+//     { id: 3, name: 'Swift' },
+//     { id: 4, name: 'Solidity' },
+// ];
 
-// two different reference, two different object
-console.log(languages.includes({ id: 1, name: 'Javascript' }));
+// // two different reference, two different object
+// console.log(languages.includes({ id: 1, name: 'Javascript' }));
 
-// find, findIndex
-const languageBeFinded = languages.findIndex(function (language) {
-    return language.name === 'C#';
-});
-console.log(languageBeFinded);
+// // find, findIndex
+// const languageBeFinded = languages.findIndex((language) => {
+//     return language.name === 'C#';
+// });
+// console.log(languageBeFinded);
+
+// function sum(a, b) {
+//     return a + b;
+// }
+const sum = (a, b) => a + b;
+console.log(sum(1, 2));
