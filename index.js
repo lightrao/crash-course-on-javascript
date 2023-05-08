a3f2dbb7cf550b738a6cdb95ba457a4c4b50e04b
RAO YANG <lightrao@hotmail.com>
2023-05-08 12:31:38 +0800

    Sorting Arrays

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-const numbers = [1, 2, 3, 4];
-const joined = numbers.join('$');
-console.log(joined);
+// const numbers = [4, 2, 3, 1];
+// numbers.sort();
+// console.log(numbers);
 
-const message = "Nice to meet you, my friend.";
-const parts = message.split(' ');
-console.log(parts);
+// numbers.reverse();
+// console.log(numbers);
 
-const message2 = parts.join('-');
-console.log(message2); // url slug
+const numbers = [4, 2, 3, 1];
+numbers.sort((a, b) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    if (a === b) return 0;
+});
+console.log(numbers);
+
+// [4, 2, 3, 1]
+// [2, 4, 3, 1]
+// [2, 3, 4, 1]
+// [2, 3, 1, 4]
+// [2, 3, 1, 4]
+// [2, 1, 3, 4]
+// [1, 2, 3, 4]
+
+// const courses = [
+//     { id: 1, name: "Node.js" },
+//     { id: 2, name: "javascript" },
+//     { id: 3, name: "Solidity" },
+//     { id: 4, name: "python" }
+// ];
+// courses.sort(function (a, b) {
+//     if (a.name.toUpperCase() < b.name.toUpperCase()) return -1;
+//     if (a.name.toUpperCase() > b.name.toUpperCase()) return 1;
+//     return 0;
+// });
+// console.log(courses);
