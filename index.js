74c94fe491e38485eb5f00c97194fbf2abe9b6d4
RAO YANG <lightrao@hotmail.com>
2023-05-08 08:37:53 +0800

    The Spread Operator

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 // const first = [1, 2, 3];
 // const second = [4, 5, 6];
-// const combined = first.concat(second);
-// console.log(combined);
-
-// const slice = combined.slice(2, 4);
-// console.log(slice);
-
-const first = [{ id: 1 }, { id: 2 }];
+const first = [{ id: 1 }, { id: 2 }, { id: 3 }];
 const second = [4, 5, 6];
-const combined = first.concat(second);
-console.log(combined);
-// first[1]['id'] = 20;
-// console.log(combined);
 
-const slice = combined.slice(1, 3);
-console.log(slice);
-first[1].id = 20;
-console.log(slice);
+const combined = [...first, 'a', ...second, 'b'];
+// first[0].id = 10;
+console.log(combined);
+
+// const copy = combined.slice();
+const copy = [...combined];
+first[0].id = 10;
+console.log(copy);
