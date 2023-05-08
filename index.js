6893a1d97af0d602229d099d5980867fb70440fb
RAO YANG <lightrao@hotmail.com>
2023-05-08 08:54:41 +0800

    Iterating an Array

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// const first = [1, 2, 3];
-// const second = [4, 5, 6];
-const first = [{ id: 1 }, { id: 2 }, { id: 3 }];
-const second = [4, 5, 6];
+const myArray = ['a', 'b', 'c', 'd'];
 
-const combined = [...first, 'a', ...second, 'b'];
-// first[0].id = 10;
-console.log(combined);
+// for (let e of myArray) {
+//     console.log(e);
+// }
 
-// const copy = combined.slice();
-const copy = [...combined];
-first[0].id = 10;
-console.log(copy);
+// for (let index in myArray) {
+//     console.log(index, myArray[index]);
+// }
+
+myArray.forEach(function (e, index) {
+    console.log(index, e);
+});
