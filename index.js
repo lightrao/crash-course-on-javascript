c4e9acd54eafcc7d120a608c0d88b3918d246faa
RAO YANG <lightrao@hotmail.com>
2023-05-11 07:01:54 +0800

    Testing the Elements of an Array

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-// const numbers = [4, 2, 3, 1];
-// numbers.sort();
-// console.log(numbers);
+const number = [1, -1, 2, 3];
 
-// numbers.reverse();
-// console.log(numbers);
-
-const numbers = [4, 2, 3, 1];
-numbers.sort((a, b) => {
-    if (a < b) return -1;
-    if (a > b) return 1;
-    if (a === b) return 0;
+const allPositive = number.every(function (value) {
+    console.log(value);
+    return value > 0;
 });
-console.log(numbers);
+console.log(allPositive);
 
-// [4, 2, 3, 1]
-// [2, 4, 3, 1]
-// [2, 3, 4, 1]
-// [2, 3, 1, 4]
-// [2, 3, 1, 4]
-// [2, 1, 3, 4]
-// [1, 2, 3, 4]
-
-// const courses = [
-//     { id: 1, name: "Node.js" },
-//     { id: 2, name: "javascript" },
-//     { id: 3, name: "Solidity" },
-//     { id: 4, name: "python" }
-// ];
-// courses.sort(function (a, b) {
-//     if (a.name.toUpperCase() < b.name.toUpperCase()) return -1;
-//     if (a.name.toUpperCase() > b.name.toUpperCase()) return 1;
-//     return 0;
-// });
-// console.log(courses);
+const number2 = [-1, -2, -3, 4, 5, 6, -7];
+const atLeastOnePositive = number2.some(function (value) {
+    console.log(value);
+    return value > 0;
+});
+console.log(atLeastOnePositive);
