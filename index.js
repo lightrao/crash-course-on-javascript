1c6cdb51c76c0763b4c3db1a6a9fc9add55af6e6
RAO YANG <lightrao@hotmail.com>
2023-05-11 07:09:45 +0800

    Filtering an Array

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const number = [1, -1, 2, 3];
+const numbers = [1, -1, 2, 3];
 
-const allPositive = number.every(function (value) {
-    console.log(value);
-    return value > 0;
-});
-console.log(allPositive);
+const justPositive = numbers.filter(number => number > 0);
 
-const number2 = [-1, -2, -3, 4, 5, 6, -7];
-const atLeastOnePositive = number2.some(function (value) {
-    console.log(value);
-    return value > 0;
-});
-console.log(atLeastOnePositive);
+console.log(justPositive);
