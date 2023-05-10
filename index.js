973ed5e0f21bf7962f17af12cfda5a02b4ec96d0
RAO YANG <lightrao@hotmail.com>
2023-05-11 07:28:38 +0800

    Mapping an Array

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const numbers = [1, -1, 2, 3];
+const numbers = [1, 2, 3, 4];
 
-const justPositive = numbers.filter(number => number > 0);
+const doubledNumbers = numbers.map(number => number * 2);
+console.log(doubledNumbers);
 
-console.log(justPositive);
+const sqrt = numbers.map(number => Math.sqrt(number));
+console.log(sqrt);
+
+const objects = numbers.map(number => ({ value: number }));
+console.log(objects);
