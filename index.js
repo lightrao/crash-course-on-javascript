bbfb49a3cf595cb7c54f35bbf3c3570a3ce7d5d8
RAO YANG <lightrao@hotmail.com>
2023-05-12 07:47:16 +0800

    Exercise 1- Array from Range

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const numbers = [1, 2, 3, 4];
+function arrayFromRang(min, max) {
+    const result = [];
+    for (let i = min; i <= max; i++) {
+        result.push(i);
+    }
+    return result;
+}
 
-// let sum = 0;
-// for (number of numbers) {
-//     sum += number;
-// }
-// console.log(sum);
-
-// a for accumulator, c for currentValue
-// a = 0, c = 1 => a = 1
-// a = 1, c = 2 => a = 3
-// a = 3, c = 3 => a = 6
-// a = 6, c = 4 => a = 10
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log(sum);
+const numbers = arrayFromRang(-10, -1);
+console.log(numbers);
 
 
