eddeb2418061b0848be75b23836a24481ef8828f
RAO YANG <lightrao@hotmail.com>
2023-05-17 07:59:50 +0800

    The Rest Operator

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-let x = 1;
-x = 'hi';
+function sum(...args) {
+    // console.log(args);
 
-function sum(/* a, b */) {
-    let total = 0;
-    for (key in arguments) {
-        total += arguments[key];
-    }
-    return total;
+    // let total = 0;
+    // for (e of args) {
+    //     total += e;
+    // }
+    // return total;
 
-    // console.log(arguments);
-    // return a + b;
+    return args.reduce((a, c) => (a + c));
 }
-console.log(sum(1, 2));
-console.log(sum(1));
-console.log(sum(1, 2, 3, 4));
+console.log(sum(1, 2, 3, 4, 5));
+
+function realPrice(discount, ...prices) {
+    // console.log(discount);
+    // console.log(prices);
+
+    const total = prices.reduce((a, c) => (a + c));
+    return total * (1 - discount);
+}
+console.log(realPrice(0.1, 20, 30));
