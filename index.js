722fdfef184d6ca0b7ac890c45d326d5031708ad
RAO YANG <lightrao@hotmail.com>
2023-05-17 07:21:20 +0800

    Function Declarations vs. Expressions

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// This is a comment
-console.log("Hello world!");
+// Function Declaration
+function walk() {
+    console.log("walk");
+}
+walk();
+
+// Function Expression
+function run() {
+    console.log("run");
+}
+run();
+const move = run;
+move();
+
+
