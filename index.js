38cb4577cb6f3385427c7174ebc7c9955dcba260
RAO YANG <lightrao@hotmail.com>
2023-05-18 07:04:19 +0800

    Local vs. Global Scope

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const person = {
-    firstName: "Albert",
-    lastName: "Einstein",
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`;
-    },
-    set fullName(value) {
-        if (typeof value !== 'string') {
-            throw new Error('You should supply a string for full name!');
-        }
-        const parts = value.split(' ');
-        if (parts.length !== 2) {
-            throw new Error('You must enter first name and last name!');
-        }
-        this.firstName = parts[0];
-        this.lastName = parts[1];
-    }
-};
-try {
-    // person.fullName = 11;
-    // person.fullName = "";
-    person.fullName = "Leonhard Euler";
-} catch (error) {
-    console.log(error);
-}
-console.log(person.fullName)
+const color = 'red';
 
+function start() {
+    const message = 'hi';
+
+    if (true) {
+        const another = 'bye';
+    }
+    // console.log(another);
+
+    for (let i = 0; i < 5; i++) {
+
+    }
+    // console.log(i);
+
+    const myString = 'Apple';
+    // const color = 'blue';
+    console.log(color);
+}
+// console.log(message);
+
+function stop() {
+    const myString = "Orange";
+}
+start();
