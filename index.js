112800c6aabeecbb6b095a75a09bc84403bf62ce
RAO YANG <lightrao@hotmail.com>
2023-05-19 07:50:57 +0800

    Changing This

--- index.js
+++ index.js
@@ -3,15 +3,25 @@
 // regular function -> global object (window, global)
 
 const movie = {
-    title: 'Your Name.',
-    tags: ['animation', 'drama', 'fantasy'],
-    showTags() {
-        const self = this;
-        this.tags.forEach(function (tag) {
-            console.log(self.title, tag);
-        });
-    }
+  title: "Your Name.",
+  tags: ["animation", "drama", "fantasy"],
+  showTags() {
+    // this.tags.forEach(function (tag) {
+    //     console.log(this.title, tag);
+    // }.bind(this));
+
+    // arrow functions inherit this from the containing function
+    this.tags.forEach((tag) => console.log(this.title, tag));
+  },
 };
 movie.showTags();
 
+// function showThis(a, b) {
+//     console.log(a, b, this);
+// }
+// showThis(1, 2);
+// showThis.call({ name: "Neo" }, 1, 2);
+// showThis.apply({ name: "Morpheus", }, [3, 4]);
 
+// const showCustomThis = showThis.bind({ name: "Trinity" });
+// showCustomThis(5, 6);
