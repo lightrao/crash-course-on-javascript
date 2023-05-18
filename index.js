00dfb0d53834daf73454b6b847cff124c84bf093
RAO YANG <lightrao@hotmail.com>
2023-05-19 07:31:21 +0800

    1st approach

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-// this: the object that is executing the current function
+// this references the object that is executing the current function
 // object have method -> object itself
 // regular function -> global object (window, global)
-
-// const video = {
-//     title: 'How to make a cake',
-//     play() {
-//         console.log(this);
-//     }
-// };
-// video.play();
-
-// video.stop = function () {
-//     console.log(this);
-// };
-// video.stop();
-
-// function showThis() {
-//     console.log(this);
-// }
-// showThis();
-
-// function Video(title) {
-//     this.title = title;
-//     console.log(this);
-// }
-// const video2 = new Video('b')
 
 const movie = {
     title: 'Your Name.',
     tags: ['animation', 'drama', 'fantasy'],
     showTags() {
+        const self = this;
         this.tags.forEach(function (tag) {
-            console.log(this.title, tag);
-        }, this);
+            console.log(self.title, tag);
+        });
     }
 };
 movie.showTags();
