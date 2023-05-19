43a1e6f20e109a7ce7ec753bb1c45f31c0687b09
RAO YANG <lightrao@hotmail.com>
2023-05-19 08:03:46 +0800

    Exercise 1- Sum of Arguments

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-// this references the object that is executing the current function
-// object have method -> object itself
-// regular function -> global object (window, global)
+// sum(1,2,3,4) -> 10
+// sum([1,2,3,4]) -> 10
 
-const movie = {
-  title: "Your Name.",
-  tags: ["animation", "drama", "fantasy"],
-  showTags() {
-    // this.tags.forEach(function (tag) {
-    //     console.log(this.title, tag);
-    // }.bind(this));
+function sum(...items) {
+  if (items.length === 1 && Array.isArray(items[0])) {
+    items = [...items[0]];
+  }
 
-    // arrow functions inherit this from the containing function
-    this.tags.forEach((tag) => console.log(this.title, tag));
-  },
-};
-movie.showTags();
-
-// function showThis(a, b) {
-//     console.log(a, b, this);
-// }
-// showThis(1, 2);
-// showThis.call({ name: "Neo" }, 1, 2);
-// showThis.apply({ name: "Morpheus", }, [3, 4]);
-
-// const showCustomThis = showThis.bind({ name: "Trinity" });
-// showCustomThis(5, 6);
+  return items.reduce((a, c) => a + c);
+}
+console.log(sum(1, 2, 3, 4));
+console.log(sum([1, 2, 3, 4]));
