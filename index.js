a69f246572f8a133a570f8672fce13d73c1b1276
RAO YANG <lightrao@hotmail.com>
2023-05-22 07:25:33 +0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const languages = [
-    { id: 1, name: 'Javascript' },
-    { id: 2, name: 'Python' },
-    { id: 3, name: 'Swift' },
-    { id: 4, name: 'Solidity' },
+  { id: 1, name: "Javascript" },
+  { id: 2, name: "Python" },
+  { id: 3, name: "Swift" },
+  { id: 4, name: "Solidity" },
 ];
 
 // two different reference, two different object
-console.log(languages.includes({ id: 1, name: 'Javascript' }));
+console.log(languages.indexOf({ id: 1, name: "Javascript" }));
+console.log(languages.includes({ id: 1, name: "Javascript" }));
 
 // find, findIndex
 const languageBeFinded = languages.findIndex(function (language) {
-    return language.name === 'C#';
+  return language.name === "C#";
 });
 console.log(languageBeFinded);
